d6be7256af577fb274127b045f7893064f20ea32
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-06-09 21:20:36 -0700

    init

---
+++ option-ext.js
@@ -0,0 +1,17 @@
+( function() {
+    // Array.prototype.contains = function contains(needle, index) {
+    //     if (!index) {
+    //         index = 0;
+    //     }
+    //     if (!this[index]) {
+    //         return false;
+    //     } else {
+    //         if (this[index] == needle) {
+    //             return true;
+    //         } else {
+    //             return this.contains(needle, index+1);
+    //         }
+    //     }
+    // }
+    // console.log(Array.prototype)
+} () )
---
+++ option-o.js
@@ -0,0 +1,254 @@
+( function(global) {
+    ø = function(id) {
+        var elements = [],
+            element, type;
+        //o: ø
+        //p: π
+
+        if (id[0] == '#') {
+            var identifier = id.slice(1);
+            element = document.getElementById(identifier);
+                elements.push(element);
+        } else if (id[0] == '.') {
+            var identifier = id.slice(1);
+            element = document.getElementsByClassName(identifier);
+            for (var i = 0; i < element.length; i++) {
+                elements.push(element[i]);
+            }
+
+        } else {
+            element = document.getElementsByTagName(id);
+            for (var i = 0; i < element.length; i++) {
+                elements.push(element[i]);
+            }
+        }
+        // var element = document.getElementById(id);
+
+        //Object for ø
+        function ø () {
+            this.element = elements;
+            // this.initialized = true;
+        }
+
+        /////////////////////////////////////////////////
+        /////////////////////////////////////////////////
+        //SELECTION
+        ø.prototype.select = function(str) {
+            var selected = [];
+            if (str[0] == '#') {
+                elements = [];
+                var identifier = id.slice(1);
+                element = document.getElementById(identifier);
+                elements.push(element);
+            } else if (str[0] == '.' ) {
+                var tag = str.slice(1);
+                for (var ele in elements) {
+                    element = elements[ele].getElementsByClassName(tag);
+                    for (var i = 0; i < element.length; i++) {
+                        selected.push(element[i]);
+                    }
+                }
+                elements = selected;
+            } else {
+                for (var ele in elements) {
+                    element = elements[ele].getElementsByTagName(str);
+                    for (var i = 0; i < element.length; i++) {
+                        selected.push(element[i]);
+                    }
+                }
+                elements = selected;
+            }
+            return new ø();
+        }
+        ø.prototype.first = function(str) {
+            if (str[0] == '#') {
+                elements = [];
+                var identifier = id.slice(1);
+                element = document.getElementById(identifier);
+                elements.push(element);
+            } else if (str[0] == '.' ) {
+            var identifier = id.slice(1);
+                element = elements[0].getElementsByClassName(identifier)[0];
+                elements = [element];
+            } else {
+                element = elements[0].getElementsByClassName(str)[0];
+                elements = [element];
+                elements = selected;
+            }
+        }
+
+
+        /////////////////////////////////////////////////
+        /////////////////////////////////////////////////
+        //SCROLLING
+        ø.prototype.popTo = function(identifier) {
+            var scroll = 0,
+                iden,
+                scrollToThis;
+
+            if (identifier[0] == "#") {
+                iden = identifier.slice(1);
+                scrollToThis = document.getElementById(iden);
+
+                scroll = scrollToThis.offsetTop;
+                window.scrollTo(0,scroll);
+            } else if (identifier[0] == ".") {
+                iden = identifier.slice(1);
+                scrollToThis = document.getElementsByClassName(iden)[0];
+
+                scroll = scrollToThis.offsetTop;
+                window.scrollTo(0,scroll);
+            } else {
+                iden = identifier;
+                scrollToThis = document.getElementsByTagName(iden)[0];
+
+                scroll = scrollToThis.offsetTop;
+                window.scrollTo(0,scroll);
+            }
+
+            return this;
+        }
+        ø.prototype.scrollTo = function(identifier, duration) {
+            var start       = global.pageYOffset,
+                to          = 0,
+                distance    = 0,
+                dpt         = 100,
+                current     = global.pageYOffset,
+                toElement;
+
+                if (identifier[0] == "#") {
+                    sliced = identifier.slice(1);
+                    toElement = document.getElementById('first');
+                    to = toElement.offsetTop;
+                    distance = to - start;
+                    dpt = distance/duration;
+                    if (dpt < 0) {
+                        dpt = Math.floor(dpt);
+                    } else {
+                        dpt = Math.ceil(dpt);
+                    }
+                } else if (identifier[0] == '.') {
+                    sliced = identifier.slice(1);
+                    toElement = document.getElementsByClassName(sliced)[0];
+                    console.log(toElement);
+                    to = toElement.offsetTop;
+                    distance = to - start;
+                    dpt = distance/duration;
+                    if (dpt < 0) {
+                        dpt = Math.floor(dpt);
+                    } else {
+                        dpt = Math.ceil(dpt);
+                    }
+                } else {
+                    toElement = document.getElementsByTagName(identifier)[0];
+                    to = toElement.offsetTop;
+                    distance = to - start;
+                    dpt = distance/duration;
+                    if (dpt < 0) {
+                        dpt = Math.floor(dpt);
+                    } else {
+                        dpt = Math.ceil(dpt);
+                    }
+                }
+                var counter = 0;
+                var previous = global.pageYOffset;
+                var loop = setInterval(function() {
+                    if (counter > duration || (previous == current && counter != 0) ) {
+                        clearInterval(loop);
+                        counter = 0;
+                    }
+                    previous = global.pageYOffset;
+                    counter++;
+
+                    window.scrollTo(0, current+dpt);
+                    current = global.pageYOffset;
+                }, 1)
+
+            console.log(start);
+            return this;
+        }
+
+        /////////////////////////////////////////////////
+        /////////////////////////////////////////////////
+        //EVENT LISTENERS
+        ø.prototype.click = function(callback) {
+            for (ele in elements) {
+                elements[ele].addEventListener("click", function() {
+                    callback();
+                })
+            }
+
+            return this;
+        }
+        ø.prototype.hover = function(hovIn, hovOut) {
+            for (ele in elements) {
+                elements[ele].addEventListener("mouseover", function() {
+                    hovIn();
+                })
+                elements[ele].addEventListener("mouseout", function() {
+                    hovOut();
+                })
+            }
+
+            return this;
+        }
+
+        /////////////////////////////////////////////////
+        /////////////////////////////////////////////////
+        //CSS PROPERTIES, GET AND SET
+        ø.prototype.width = function(width) {
+            for (ele in elements) {
+                if (width) {
+                    if (typeof width === 'number') {
+                        width += 'px';
+                    }
+                    elements[ele].style.width = width;
+                } else {
+                    console.log('Width:', elements[ele].offsetWidth);
+                }
+            }
+
+            return this;
+        }
+        ø.prototype.height = function(height) {
+            for (ele in elements) {
+                if (height) {
+                    if (typeof height === 'number') {
+                        height += 'px';
+                    }
+                    elements[ele].style.height = height;
+                } else {
+                    console.log('Height:', elements[ele].offsetHeight);
+                }
+            }
+            // console.log(elements[ele].offsetHeight);
+            return this;
+        }
+        ø.prototype.attr = function(attr, value) {
+            for (ele in elements) {
+                if (value && typeof attr == 'string') {
+                    elements[ele].style[attr] = value;
+                } else {
+                    console.log(elements[ele].style[attr]);
+                }
+            }
+
+            return this;
+        }
+        ø.prototype.background = function(background) {
+            for (ele in elements) {
+                if (background) {
+                    elements[ele].style.background = background;
+                } else {
+                    console.log('Background:', elements[ele].style.background);
+                }
+            }
+
+            return this;
+        }
+
+        /////////////////////////////////////////////////
+        /////////////////////////////////////////////////
+        return new ø();
+    }
+} (window) )
